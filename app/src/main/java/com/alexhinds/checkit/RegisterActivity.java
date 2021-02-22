d5b5057054f4e748ca2bb7d6a19dc4a358f82639
Alex Hinds <alex.hinds1@gmail.com>
2021-02-22 13:48:52 -0500

    initial user registration and input validation complete

--- app/src/main/java/com/alexhinds/checkit/RegisterActivity.java
+++ app/src/main/java/com/alexhinds/checkit/RegisterActivity.java
@@ -7,29 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.basgeekball.awesomevalidation.AwesomeValidation;
-import com.basgeekball.awesomevalidation.ValidationStyle;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.UUID;
-
-
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String TAG = "REGISTER_ACTIVITY"; // for logging
@@ -59,7 +48,7 @@ public class RegisterActivity extends AppCompatActivity {
         final AwesomeValidation awesomeValidation = new AwesomeValidation(ValidationStyle.COLORATION);
         String regexPassword = "[a-zA-Z0-9]+";
         awesomeValidation.addValidation(registerUserName, regexPassword, "Enter valid email address (alphanumeric only)");
-        //mAwesomeValidation.addValidation(loginEmail, Patterns.EMAIL_ADDRESS, "Enter valid email address");
+        //awesomeValidation.addValidation(loginEmail, Patterns.EMAIL_ADDRESS, "Enter valid email address");
         awesomeValidation.addValidation(registerPassword, regexPassword, "Enter valid password (alphanumeric only)");
 
         //alreadyRegistered Onclick
@@ -79,7 +68,7 @@ public class RegisterActivity extends AppCompatActivity {
                 String username = registerUserName.getText().toString();
                 String password = registerPassword.getText().toString();
 
-                // create account with User object to store associated info
+                // if valid input, create account with User object to store associated info
                 if (awesomeValidation.validate()) {
                     User user = new User(username, password);
                     createAccount(user);
