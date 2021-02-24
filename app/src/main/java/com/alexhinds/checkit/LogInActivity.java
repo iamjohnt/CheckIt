e78157a15e008672ffec17db67c6b63dc27d3e6c
ahinds1 <ahinds1@bu.edu>
2021-02-24 16:06:33 -0500

    refactored login using firebase auth

--- app/src/main/java/com/alexhinds/checkit/LogInActivity.java
+++ app/src/main/java/com/alexhinds/checkit/LogInActivity.java
@@ -1,61 +1,112 @@
 package com.alexhinds.checkit;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LogInActivity extends AppCompatActivity {
+    private final String TAG = "LOGIN_ACTIVITIY"; // for logging
+
 
     // Define Resources
-    private EditText loginUsername;
+    private EditText loginUserEmail;
     private EditText loginPassword;
     private Button loginButton;
     private TextView notRegistered;
 
-    private AwesomeValidation mAwesomeValidation;
+
+
+
+    private AwesomeValidation awesomeValidation;
     private DatabaseReference database;
+    private FirebaseAuth auth;
+
+    // Login Function
+    private void attemptLogin(String loginUserEmail, String loginPassword) {
+
+        database = FirebaseDatabase.getInstance().getReference();
+        auth = FirebaseAuth.getInstance();
+
+        auth.signInWithEmailAndPassword(loginUserEmail, loginPassword)
+                .addOnSuccessListener(authResult -> {
+                    Log.d(TAG, "attemptLogin: Successful");
+
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                })
+                .addOnFailureListener(e -> {
+                    Log.d(TAG, "attemptLogin: Failed: " + e.getMessage());
+                });
+        // TODO: connect to database & validate info
+    }
+
+
+    // ANDROID LIFE CYCLE
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = auth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            auth.signOut();
+            //startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+        else if (currentUser == null) {
+            Toast.makeText(getApplicationContext(),
+                    "currentUser is null",
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-
+        auth = FirebaseAuth.getInstance();
 
         // Get Resources
-        loginUsername = (EditText) findViewById(R.id.login_user_name);
+        loginUserEmail = (EditText) findViewById(R.id.login_user_email);
         loginPassword = (EditText) findViewById(R.id.login_password);
         loginButton = (Button) findViewById(R.id.login_button);
         notRegistered = (TextView) findViewById(R.id.new_user);
 
         // validation configuration
-        final AwesomeValidation mAwesomeValidation = new AwesomeValidation(ValidationStyle.COLORATION);
-        String regexPassword = "[a-zA-Z0-9]+";
+        final AwesomeValidation awesomeValidation = new AwesomeValidation(ValidationStyle.COLORATION);
+        String regexPassword = "[a-zA-Z0-9]{6,}";
 
-        mAwesomeValidation.addValidation(loginUsername, regexPassword, "Enter valid email address (alphanumeric only)");
-       //mAwesomeValidation.addValidation(loginEmail, Patterns.EMAIL_ADDRESS, "Enter valid email address");
 
-        mAwesomeValidation.addValidation(loginPassword, regexPassword , "Enter valid password (alphanumeric only)");
+
+        awesomeValidation.addValidation(loginUserEmail, Patterns.EMAIL_ADDRESS, "Enter a valid email address");
+
+        awesomeValidation.addValidation(loginPassword, regexPassword , "Enter valid password (alphanumeric only, length 6+)");
+
 
         // Login Button OnClick
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (mAwesomeValidation.validate()) {
-                    attemptLogin(loginUsername.getText().toString(), loginPassword.getText().toString());
+                if (awesomeValidation.validate()) {
+                    attemptLogin(loginUserEmail.getText().toString(), loginPassword.getText().toString());
                 }
             }
 
@@ -70,11 +121,10 @@ public class LogInActivity extends AppCompatActivity {
 
     }
 
-
-    // Login Function
-    private void attemptLogin(String loginUsername, String loginPassword) {
-        database = FirebaseDatabase.getInstance().getReference();
-
-        // TODO: connect to database & validate info
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //auth.signOut();
     }
+
 }
--- app/src/main/java/com/alexhinds/checkit/MainActivity.java
+++ app/src/main/java/com/alexhinds/checkit/MainActivity.java
@@ -1,14 +1,35 @@
 package com.alexhinds.checkit;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MAIN_ACTIVITY";
+    private FirebaseAuth auth;
+    private TextView mainActivityUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+
+        mainActivityUserName = findViewById(R.id.displayName);
+        String displayName = auth.getCurrentUser().getDisplayName();
+        Log.d(TAG, "onCreate: displayName = " + displayName);
+        mainActivityUserName.setText(displayName);
     }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        auth.signOut();
+    }
+
 }
