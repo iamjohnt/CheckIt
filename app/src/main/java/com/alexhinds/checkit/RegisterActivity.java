ea8f8674766e88b19b95c3c6903b3a342e956062
ahinds1 <ahinds1@bu.edu>
2021-03-03 12:37:45 -0500

    RegisterActivity redirects to LoginActivity on account creation

--- app/src/main/java/com/alexhinds/checkit/RegisterActivity.java
+++ app/src/main/java/com/alexhinds/checkit/RegisterActivity.java
@@ -10,16 +10,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -48,7 +42,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         auth = FirebaseAuth.getInstance();
-
+        database = FirebaseDatabase.getInstance().getReference("test/users"); // test db
         //Get Resources
         registerUserEmail = (EditText) findViewById(R.id.register_user_email);
         registerUserName = (EditText) findViewById(R.id.register_user_name);
@@ -56,13 +50,13 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton = (Button) findViewById(R.id.register_button);
         alreadyRegistered = (TextView) findViewById(R.id.already_registered);
 
-        //
+        // validation rules
         final AwesomeValidation awesomeValidation = new AwesomeValidation(ValidationStyle.COLORATION);
         String regexUserName = "^[a-zA-Z0-9]{2,20}$";
-        String regexPassword =  "^[a-zA-Z0-9]{6,}$";
-        awesomeValidation.addValidation(registerUserName, regexUserName, "Enter valid username (alphanumeric only, req length 2-20");
+        String regexPassword =  "^[a-zA-Z0-9]{6,20}$";
+        awesomeValidation.addValidation(registerUserName, regexUserName, "Enter valid username (alphanumeric only, length 2-20)");
         awesomeValidation.addValidation(registerUserEmail, Patterns.EMAIL_ADDRESS, "Enter valid email address");
-        awesomeValidation.addValidation(registerPassword, regexPassword, "Enter valid password (alphanumeric only) req length 6+");
+        awesomeValidation.addValidation(registerPassword, regexPassword, "Enter valid password (alphanumeric only length 6-20)");
 
         //alreadyRegistered Onclick
         alreadyRegistered.setOnClickListener(new View.OnClickListener() {
@@ -77,18 +71,16 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // get registration info in string format
-                String userEmail = registerUserEmail.getText().toString();
-                String userName = registerUserName.getText().toString();
-                String password = registerPassword.getText().toString();
 
-                // if valid input, create account with User object to store associated info
+
+                // if valid input, create account with associated info
                 if (awesomeValidation.validate()) {
-                    createAccount(userEmail, userName, password);
-                    // continue to login activity
-                    startActivity(new Intent(getApplicationContext(), LogInActivity.class));
-                    finish();
+                    // registration info
+                    String userEmail = registerUserEmail.getText().toString();
+                    String userName = registerUserName.getText().toString();
+                    String password = registerPassword.getText().toString();
 
+                    createAccount(userEmail, userName, password);
                 }
 
             }
@@ -97,12 +89,12 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    /** createAccount                   creates Firebase Auth User and user entry
-     *                                  in the database
+    /** createAccount                   creates Firebase Application User and user entry
+     *                                  in the realtime database
      *
-     * @param userEmail email used to create account
+     * @param userEmail email used to create account with Firebase Authentication
      * @param username username used for displayName in the app
-     * @param password user account password
+     * @param password password used to create account with Firebase Authentication
      */
 
     private void createAccount(String userEmail, String username, String password) {
@@ -111,69 +103,63 @@ public class RegisterActivity extends AppCompatActivity {
         final String userName = username;
         final String userPassword = password;
 
+        final String TAG = "createAccount: ";
 
-        final String USER_PROFILE_TAG = "USER_PROFILE_UPDATE";
+
 
         // sign up
         auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(RegisterActivity.this, task -> {
-                    if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                       FirebaseUser currUser = FirebaseAuth.getInstance().getCurrentUser(); // get the newly created user
-                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder() // create profile update to set username as display name
+                // if User creation is successful, update their display name, and add their entry in the database
+                .addOnSuccessListener(RegisterActivity.this, authResult -> {
+
+                    Log.d(TAG, "createUserWithEmail:success ");
+                    Toast.makeText(getApplicationContext(), "Account Created Successfully...",
+                            Toast.LENGTH_SHORT);
+
+                    // get the newly created user
+                    FirebaseUser currUser = authResult.getUser();
+
+
+                    // set Firebase User's display name to the supplied username
+                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder() // create profile update to set username as display name
                                 .setDisplayName(userName).build();
 
-                        Log.d(TAG, "createUserWithEmail:success ");
+                    final String USER_PROFILE_TAG = "USER_PROFILE_UPDATE";
+                    currUser.updateProfile(profileUpdates) // update profile with the change request
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Log.d(USER_PROFILE_TAG, "Name updated to \'"
+                                        + userName
+                                        + "\'.");
+                                Toast.makeText(getApplicationContext(),
+                                        "Name updated to " + userName,
+                                        Toast.LENGTH_LONG).show();
+                            }
 
-                        Toast.makeText(getApplicationContext(), "Account Created Successfully...",
-                                Toast.LENGTH_SHORT);
-
-
-                        currUser.updateProfile(profileUpdates) // update profile
-                                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-                                        if (task.isSuccessful()) {
-                                            Log.d(USER_PROFILE_TAG, "Name updated to \'"
-                                                    + userName
-                                                    + "\'.");
-                                            Toast.makeText(getApplicationContext(),
-                                                            "Name updated to " + userName,
-                                                            Toast.LENGTH_LONG).show();
-                                        }
-
-                                    }
                         });
 
+                // ADDING USER TO REALTIME DB
 
-                    } else {
-                        // If sign up fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                        Toast.makeText(getApplicationContext(), "Account Creation Failed.\n" + task.getException() ,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                    database = FirebaseDatabase.getInstance().getReference("test/users"); // test db
-
-
-                    // set up entry for user in db
-                    User user = new User(userName, userPassword);
+                    User user = new User(userName, userPassword); // set up entry for user in db
                     // write object to database, using UID from signing up as the unique/primary key - it handles conversion to JSON
-                    database.child(auth.getCurrentUser().getUid()).setValue(user)
+                    database.child(currUser.getUid()).setValue(user)
 
-                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Log.d(TAG, "createAccount::onSuccess: Account Created");
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.d(TAG, "createAccount::onFailure: Account Creation Failed");
-                                }
-                            });
+                            .addOnSuccessListener(aVoid -> Log.d(TAG, "createAccount::onSuccess: User added to DB"))
+                            .addOnFailureListener(e -> Log.d(TAG, "createAccount::onFailure: User NOT added to DB"));
+
+                    // go to login activity
+                    startActivity(new Intent(getApplicationContext(), LogInActivity.class));
+                    finish();
+                })
+
+                // account creation failed - ( FirebaseAuthUserCollisionException - email already in use)
+                .addOnFailureListener(RegisterActivity.this, e -> {
+                    Log.w(TAG, "createUserWithEmail:failure" + e.getMessage());
+                    Toast.makeText(getApplicationContext(), "Account Creation Failed.\n" + e.getMessage(),
+                            Toast.LENGTH_SHORT).show();
 
                 });
+
     }
 
     protected void onStart() {
@@ -181,7 +167,6 @@ public class RegisterActivity extends AppCompatActivity {
         FirebaseUser currentUser = auth.getInstance().getCurrentUser();
         if (currentUser != null) {
             auth.getInstance().signOut();
-            finish();
         }
         else if (currentUser == null) {
             Toast.makeText(getApplicationContext(),
