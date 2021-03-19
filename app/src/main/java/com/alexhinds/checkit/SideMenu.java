43e690dcb3fb22eb4f212e55d397ca904ca0e870
petererian <erianp@bu.edu>
2021-03-19 18:11:49 -0400

    Added Class SideMenu

---
+++ app/src/main/java/com/alexhinds/checkit/SideMenu.java
@@ -0,0 +1,16 @@
+package com.alexhinds.checkit;
+
+import android.view.Menu;
+
+public class SideMenu {
+
+    public Menu menu;
+
+    public SideMenu(Menu menu){
+        this.menu = menu;
+
+    }
+
+
+
+}
