bc40ff4889a4e3147a7385bf2884820d07f9a24f
Stephanie Lin <stephanie.lin@uconn.edu>
2021-04-01 22:11:39 -0400

    changed activity scenario to a rule

--- app/src/androidTest/java/com/alexhinds/checkit/LoginTest.java
+++ app/src/androidTest/java/com/alexhinds/checkit/LoginTest.java
@@ -4,8 +4,10 @@ import android.os.SystemClock;
 import android.util.Log;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -25,6 +27,11 @@ import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentat
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class LoginTest {
+
+    // Start from Login Page
+    @Rule
+    public ActivityScenarioRule<LogInActivity> logInActivityActivityScenarioRule = new ActivityScenarioRule<>(LogInActivity.class);
+
 
     /*  runs app, with Login Activity as the launcher activity (first activity to run)
      *   then runs infinite loop, so you can continue to manually test (otherwise the method returns, and the app is closed */
@@ -46,38 +53,32 @@ public class LoginTest {
 
     @Test
     public void test_login_title_visible() {
-        ActivityScenario<LogInActivity> test = ActivityScenario.launch(LogInActivity.class);
         onView(withId(R.id.loginTitle)).check(matches(isDisplayed()));
     }
 
     @Test
     public void test_login_userEmail_textField_visible() {
-        ActivityScenario<LogInActivity> test = ActivityScenario.launch(LogInActivity.class);
         onView(withId(R.id.login_user_email)).check(matches(isDisplayed()));
     }
 
     @Test
     public void test_login_password_textField_visible() {
-        ActivityScenario<LogInActivity> test = ActivityScenario.launch(LogInActivity.class);
         onView(withId(R.id.login_password)).check(matches(isDisplayed()));
     }
 
     @Test
     public void test_login_button_visible() {
-        ActivityScenario<LogInActivity> test = ActivityScenario.launch(LogInActivity.class);
         onView(withId(R.id.login_button)).check(matches(isDisplayed()));
     }
 
     @Test
     public void test_login_newUser_visible() {
-        ActivityScenario<LogInActivity> test = ActivityScenario.launch(LogInActivity.class);
         onView(withId(R.id.new_user)).check(matches(isDisplayed()));
     }
 
 
     @Test
     public void test_login_images_visible() {
-        ActivityScenario<LogInActivity> test = ActivityScenario.launch(LogInActivity.class);
         onView(withId(R.id.tasks_solid)).check(matches(isDisplayed()));
         onView(withId(R.id.check_double_solid)).check(matches(isDisplayed()));
         onView(withId(R.id.check)).check(matches(isDisplayed()));
@@ -86,9 +87,6 @@ public class LoginTest {
 
     @Test
     public void test_login_newUser() {
-        // Start at Login
-        ActivityScenario<LogInActivity> test = ActivityScenario.launch(LogInActivity.class);
-
         // Click on New User
         onView(withId(R.id.new_user)).perform(click());
 
@@ -101,9 +99,6 @@ public class LoginTest {
 
     @Test
     public void test_login_validUsernamePassword() {
-        // Start at Login
-        ActivityScenario<LogInActivity> test = ActivityScenario.launch(LogInActivity.class);
-
         // Login with Valid Test Username / Password
         onView(withId(R.id.login_user_email)).perform(typeText("test123@test.com"));
         onView(withId(R.id.login_password)).perform(typeText("test123")).perform(closeSoftKeyboard());
@@ -118,9 +113,6 @@ public class LoginTest {
 
     @Test
     public void test_login_invalidUsername() {
-        // Start at Login
-        ActivityScenario<LogInActivity> test = ActivityScenario.launch(LogInActivity.class);
-
         // Login with Valid Test Username / Password
         onView(withId(R.id.login_user_email)).perform(typeText("test"));
         onView(withId(R.id.login_password)).perform(typeText("test123")).perform(closeSoftKeyboard());
@@ -135,9 +127,6 @@ public class LoginTest {
 
     @Test
     public void test_login_invalidPassword_tooShort() {
-        // Start at Login
-        ActivityScenario<LogInActivity> test = ActivityScenario.launch(LogInActivity.class);
-
         // Login with Valid Test Username / Password
         onView(withId(R.id.login_user_email)).perform(typeText("test123@test.com"));
         onView(withId(R.id.login_password)).perform(typeText("test")).perform(closeSoftKeyboard());
@@ -149,12 +138,9 @@ public class LoginTest {
         // On Create Screen
         onView(withId(R.id.textview_welcome)).check(doesNotExist());
     }
-    
+
     @Test
     public void test_login_invalidPassword_incorrect() {
-        // Start at Login
-        ActivityScenario<LogInActivity> test = ActivityScenario.launch(LogInActivity.class);
-
         // Login with Valid Test Username / Password
         onView(withId(R.id.login_user_email)).perform(typeText("test123@test.com"));
         onView(withId(R.id.login_password)).perform(typeText("test12345")).perform(closeSoftKeyboard());
